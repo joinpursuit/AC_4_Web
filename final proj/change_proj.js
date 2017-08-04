166c3cca90581318526cf5a3a25eed69c649e2bb
Tyler Wang <tw325@cornell.edu>
2017-08-04 16:47:05 -0400

    added dynamic solution

--- final proj/change_proj.js
+++ final proj/change_proj.js
@@ -38,26 +38,37 @@ function recursiveChange(num, arr){
 
 // dynamic solution with same parameters
 // this gives the correct answer!
-// THIS IS NOT THE OPTIMAL SOLUTION! MANY CALCULATIONS ARE REPEATED.
-/* INCOMPLETE */
-// function dynamicChange(num, arr){
-//     if (num <= arr[0]){
-//         return num;
-//     } else {
-//         var min = Number.MAX_SAFE_INTEGER;
-//         for (var i = arr.length - 1; i >= 0; i--){
-//             if (num >= arr[i]){
-//                 if (dynamicChange(num - arr[i],arr) < min){
-//                     min = dynamicChange(num - arr[i],arr)
-//                 }
-//             }
-//         }
-//         return min + 1;
-//     }
-// }
+// THIS IS THE CLOSEST TO OPTIMAL OUT OF THE THREE SOLUTIONS
+function dynamicChange(num, arr){
+    var mem = Array(arr.length);
+    function helper(n){
+        if (mem[n]){
+            return mem[n];
+        } else if (n <= arr[0]){
+            mem[n] = n;
+            return n;
+        } else {
+            var min = Number.MAX_SAFE_INTEGER;
+            for (var i = arr.length - 1; i >= 0; i--){
+                if (n >= arr[i]){
+                    if (helper(n - arr[i]) < min){
+                        min = helper(n - arr[i])
+                    }
+                }
+            }
+            mem[n] = min + 1;
+            return min + 1;
+        }
+    }
+    return helper(num);
+}
 
 console.time("dbsave");  
 console.log("(num:" + 60 + ", " + "currency:[1,5,12], min:" + recursiveChange(60, [1,5,12]));
+console.timeEnd("dbsave"); 
+
+console.time("dbsave");  
+console.log("(num:" + 60 + ", " + "currency:[1,5,12], min:" + dynamicChange(60, [1,5,12]));
 console.timeEnd("dbsave"); 
 
 // for (var i = 0; i < 10; i++){ 
