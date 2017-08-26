0c0a967802cf87c26e62524a82cba7bcea40edf6
lizraeli <leo2002b@yahoo.com>
2017-08-26 11:35:20 -0400

    guess the number project

--- bridge/examples/readline.js
+++ bridge/examples/readline.js
@@ -9,29 +9,10 @@ function clear () {
   process.stdout.write('\u001B[2J\u001B[0;0f')
 }
 
-var journal = []
-
-// takes an array of strings, 
-// converts them into numbers and adds them up
-function addNumbers(arr){
-  // ['4', '5', '9']
-  var sum = 0
-  for (var i = 0; i < arr.length; i++){
-    var num = Number(arr[i])
-    if (!isNaN(num)){
-      sum += num
-    }
-  }
-  return sum
-}
-
 rl.on('line', function (input) {
   clear()
-  var arr = input.split(' ')
-  var sum = addNumbers(arr)
-  console.log(sum)
-  console.log('write some numbers: ')
+
 })
+  
 
 clear()
-console.log('write some numbers: ')
---
+++ bridge/projects/guess_the_number/guess_the_number_scaffold.js
@@ -0,0 +1,121 @@
+
+/*
+ *   Boilerplate
+ */
+'use strict';
+
+var readline = require('readline')
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+function clear () {
+  process.stdout.write('\u001B[2J\u001B[0;0f')
+}
+
+// End of boilerplate
+
+/**
+ * @function returns a random number between min and max
+ * @param  {Number} min
+ * @param  {Number} max
+ * @return {Number}
+ */
+function getRandomNumber (min, max) {
+  return Math.round(Math.random() * (max - min)) + min
+}
+
+/**
+ * @function Game
+ * @param  {Number} number {the number to guess}
+ * @param  {Number} triesLeft {number of attempts }
+ * @param  {Boolean} win
+ * @param  {String} message
+ * @return {Game} a Game object
+ */
+function Game (number, triesLeft, gameOver, message) {
+  var game = {
+    number: number,
+    triesLeft: triesLeft,
+    gameOver: gameOver,
+    message: message
+  }
+  return game
+}
+
+/**
+ * @function updateGame
+ * @param {Game}
+ * @param {Number} guess
+ * @param {Game} The updated game
+ */
+function updateGame (game, guess) {
+  var number = game.number
+  var triesLeft = game.triesLeft - 1
+  var message = ''
+  var gameOver
+
+  if (guess === number) {
+    gameOver = true
+    message = 'You win!'
+  } else if (guess < number) {
+    gameOver = false
+    message = 'Aim higher'
+  } else {
+    gameOver = false
+    message = 'Aim lower'
+  }
+  
+  var updatedGame = Game(number, triesLeft, gameOver, message)
+  return updatedGame
+}
+
+
+/**
+ *  Called when a user presses <Enter>
+ */
+rl.on('line', function (input) {
+  var guess = Number(input)
+  
+  game = updateGame(game, guess)
+  
+  if (game.gameOver){
+    render (game, '')
+    process.exit()
+  } else {
+    render(game, 'guess a number')
+  }
+});
+
+/**
+ * Logs the game message and a prompt
+ * @function
+ * @param  {Game} game   
+ * @param  {String} prompt 
+ */
+function render(game, prompt){
+  clear()
+  console.log(game.message)
+  console.log(prompt)
+}
+
+
+/**
+ * Called when the user starts the game. Sets up the game object.
+ * @function startGame 
+ *
+ */
+function startGame(){
+  var gameNumber = getRandomNumber(1, 10)
+  var tries = 10
+  var gameOver = false
+  var message = 'Guessing Number Game'
+  game = Game(gameNumber, tries, gameOver, message)
+
+  render(game, 'guess a number')
+}
+
+var game
+startGame()
