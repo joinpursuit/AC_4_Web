7fdda1f85dd77156df0b7f3673b5c71d6f0748ee
Matthew Thorry <mthorry@gmail.com>
2018-06-08 17:38:44 -0400

    new ruby file

---
+++ units/ruby_practice/4. make_a_class.rb
@@ -0,0 +1,17 @@
+# 1. Create a class called MyCar. 
+	# When you initialize a new instance or object of the class, allow the user to define some instance variables that tell us the year, color, and model of the car. 
+	# Create an instance variable that is set to 0 during instantiation of the object to track the current speed of the car as well. 
+	# Create instance methods that allow the car to speed up, brake, and shut the car off.
+
+
+
+
+# 2. Add an accessor method to your MyCar class to change and view the color of your car. 
+	# Then add an accessor method that allows you to view, but not modify, the year of your car.
+
+
+
+
+# 3. You want to create a nice interface that allows you to accurately describe the action you want your program to perform. 
+	# Create a method called spray_paint that can be called on an object and will modify the color of the car.
+
