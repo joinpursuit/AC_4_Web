fc9861bc500a570d7553a90e1fbb4b6e72bbbccd
Michelle <32519634+michellelala@users.noreply.github.com>
2018-06-08 16:41:37 -0400

    fix some minor errors

--- units/ruby_practice/1. intro.rb
+++ units/ruby_practice/1. intro.rb
@@ -1,23 +1,23 @@
-# write a function called hello that returns "hello"
+# write a function called return_hello that returns "hello"
 
-#write a funciton called say_hello that takes in a name and says "hello" and then that persons name
-#now change the function say_hello to take in the option of taking in a name. If no name is given
-  #it should return just "hello".
+# write a function called say_hello that takes in a name and says "hello" and then that persons name
+# now change the function say_hello to take in the option of taking in a name. If no name is given
+# it should return just "hello".
 
 
 # write a function called is_higher_than_ten. your function should
 # return "Lower" if lower. "Higher" if higher. and "You entered 10" if number is ten
 
-#write function fizzbuzz. Returns an array with the words fizz for every
-# number that is divisible by 3 and not 5. buzz for every number divisble by 5 and not 3.
-# And fizzbuzz for numbers that are divisible by both.
+# write function fizzbuzz. Returns an array with the words fizz for every
+# number that is divisible by 3 and not 5. buzz for every number divisible by 5 and not 3,
+# and fizzbuzz for numbers that are divisible by both.
 
 # function shout : should shout the word back at user
 
-#function repeat: takes in a word and a number of times to input. Should
+# function repeat: takes in a word and a number of times to input. Should
 # default to 2 times.
 
-#function last_word: returns last word in string.
+# function last_word: returns last word in string.
 
 # function no_lowercase : returns a string without its lowercase letters or spaces
 
@@ -52,11 +52,11 @@ if __FILE__ == $PROGRAM_NAME
   p last_word("the large brown fox") == "fox"
   p
   p "Question 8:"
-  p no_lowercase("ThEy LoVe To CodE") == "TELVTC"
+  p no_lowercase("ThEy LoVe To CodE") == "TELVTCE"
   p
   p "Question 9:"
   p vowel_count("hi everyone! ") == 5
-  p vowel_count("abcd! ") == 0
+  p vowel_count("abcd! ") == 1
   p
   p "Question 10:"
   p factorial(0) == 1
